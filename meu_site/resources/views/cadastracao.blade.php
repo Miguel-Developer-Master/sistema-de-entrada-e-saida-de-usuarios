ffbecaab29c8f62eb3b1e63a5651425f64d93f14
Miguel-Developer-Master <migueldevelopermaster@gmail.com>
2025-10-19 23:12:41 -0300

    front-end configures

---
+++ meu_site/resources/views/cadastracao.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pr-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/cadastracaoo.css', 'resources/js/cadastracaoo.js'])
+</head>
+<body>
+</body>
+</html>
