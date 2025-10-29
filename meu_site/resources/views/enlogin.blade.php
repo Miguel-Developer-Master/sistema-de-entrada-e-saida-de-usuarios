4d04391cf6b570d939c631bac53ffebf1d9c3d27
Miguel-Developer-Master <migueldevelopermaster@gmail.com>
2025-10-29 20:19:16 -0300

    english version .blade.php

---
+++ meu_site/resources/views/enlogin.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Login</title>
+  <meta name="description" content="Acess your account to enter in plataform." />
+    @vite(['resources/css/enlog-in.css', 'resources/js/enlog-in.js'])
+</head>
+<body>
+
+</body>
+</html>
---
+++ meu_site/resources/views/enregistration.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>registration</title>
+  <meta name="description" content="Acess your account to enter in plataform." />
+    @vite(['resources/css/encadastracao.css', 'resources/js/encadastramento.js'])
+</head>
+<body>
+
+</body>
+</html>
