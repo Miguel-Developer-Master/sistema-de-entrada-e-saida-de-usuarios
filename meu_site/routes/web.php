a45f5a9da4d28d5c000317ae1ebf59d94f9a5869
Miguel-Developer-Master <migueldevelopermaster@gmail.com>
2025-10-29 17:28:41 -0300

    rostas english

--- meu_site/routes/web.php
+++ meu_site/routes/web.php
@@ -6,6 +6,14 @@ Route::get('/', function () {
     return view('cadastracao');
 })->name('cadastramento');
 
+Route::get('/en/cadastramento', function () {
+    return view('cadastracao');
+})->name('/en/cadastramento');
+
 Route::get('/login', function () {
     return view('login');
 })->name('login');
+
+Route::get('/en/login', function () {
+    return view('enlogin');
+})->name('enlogin');
